8f9057fa2b47265cb5d629c35a07668efd5ec1ff
Mike Groves <mike.groves89@outlook.com>
2024-12-14 17:04:50 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import bg from './assets/bg.jpg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className='title'>Coming Soon</div>
+    </>
+  )
+}
+
+export default App
